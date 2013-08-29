2cdcaac5d30a897e7fb4adedca63960d03a2a821
Ryan Marren <rymarr475@gmail.com>
2013-08-29 19:53:24 -0400

    added dance party

--- jscript/script.js
+++ jscript/script.js
@@ -7,4 +7,16 @@ $(document).ready( function() {
 	$('.acc').mouseleave( function() {
 		$(this).css('background-color', '#AEF986');
 	});
+	$('#dance').click( function() {
+		$('#info').append("<audio autoplay='autoplay' loop='loop'><source src='dance.mp3' type='audio/mpeg'></audio>");
+		setInterval(colorSwap, 450);
+		$(this).remove();
+	});	
 });
+
+function colorSwap() {
+	$('body').css("background-color", "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")");
+	$('#page').css("background-color", "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")");
+	$('#nav').css("background-color", "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")");
+	$('.button').css("background-color", "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")");
+}
